f962e720577e1b69b00169b2a5064a02b4efffde
shivam.pandya21minus1@gmail.com <shivam.pandya9@gmail.com>
2020-11-17 21:18:04 +0530

    Update

---
+++ src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ProjectSection from "./ProjectSection";
+import BlogSection from "./BlogSection";
+import SkillsSection from "./SkillsSection";
+import RecommendationSection from "./RecommendationSection";
+import Title from "./Title";
+import About from "./About";
+
+function HomePage() {
+  return (
+    <div>
+      <Title name="YOUR NAME" leadText="I am a freelancer from India" />
+      <RecommendationSection />
+      <SkillsSection />
+      <ProjectSection />
+      <About />
+      <BlogSection />
+    </div>
+  );
+}
+
+export default HomePage;
